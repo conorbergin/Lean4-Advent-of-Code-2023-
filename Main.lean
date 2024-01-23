5a193471946889c18e5d1b8101c417bb1c927758
Conor Bergin <conorbergin@protonmail.com>
2024-01-23 01:13:25 +0000

    chnage project structure

--- Main.lean
+++ Main.lean
@@ -1,4 +1,11 @@
 import «AdventOfCode2023»
 
+def message :=
+"I am using the Lean interpreter to solve these problems
+please open one of the files in the AdventOfCode2023 folder
+in vscode or emacs with the extension and use the eval macros,
+if you want to use your own inputs, replace the strings in the
+AdventofCode2021/Input folder"
+
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println message
